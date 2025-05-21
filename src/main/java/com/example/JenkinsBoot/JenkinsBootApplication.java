003ff33e65e7090b5b19b78c3d068bb62669885c
iKoNFlair <ritikpillai8@gmail.com>
2025-05-21 17:21:44 +0530

    first commit

---
+++ src/main/java/com/example/JenkinsBoot/JenkinsBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JenkinsBoot/JenkinsBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
